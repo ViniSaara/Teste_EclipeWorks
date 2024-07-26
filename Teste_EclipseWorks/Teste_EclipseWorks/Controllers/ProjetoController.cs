f0559e62ca03782f5f1911cbcdeba54cf8378231
Vinícius Sabino <viniaraujobh2000@gmail.com>
2024-07-26 12:22:53 -0300

    Criando projeto web.

---
+++ Teste_EclipseWorks/Teste_EclipseWorks/Controllers/ProjetoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teste_EclipseWorks.Controllers
+{
+    public class ProjetoController : Controller
+    {
+        [HttpGet("api/Index")]
+        public IActionResult Index(int idProjeto)
+        {
+            return View();
+        }
+
+        [HttpGet("api/GetListaProjetos")]
+        public IActionResult GetListaProjetos(int idProjeto)
+        {
+            return Ok(new { Id = idProjeto + 1 });
+        }
+    }
+}
